27ef4e8a26dafd4072412773141133d974005147
Thorsten Sick <thorsten.sick@email.de>
2014-09-19 21:13:55 +0200

    Adding System state struct. Damn preprocessor

--- Arduino_Simon_Says/Arduino_Simon_Says.ino
+++ Arduino_Simon_Says/Arduino_Simon_Says.ino
@@ -16,6 +16,7 @@ LCD: ok
 */
 // include the library code:
 #include <LiquidCrystal.h>
+
 // SmartMaker special
 LiquidCrystal lcd(7, 6, 5, 4, 3, 2);
 
@@ -23,6 +24,7 @@ LiquidCrystal lcd(7, 6, 5, 4, 3, 2);
 const String disabled = String("disabled");
 const String enabled = String("enabled");
 const String broken = String("broken");
+const String testing = String("testing");
 String operationMode = enabled;
 
 int taskCounter = 0;
@@ -34,13 +36,89 @@ const unsigned char Box1_Button_blau = 11;  // Vakuumeinlass
 const unsigned char Box1_Schloss = 10;  // Radon
 const unsigned char Box1_LED = 13;
 const unsigned char Box2_Button_gruen = 8;  // Schrott abwurf
-const unsigned char Box2_Regler = A5;  // SitzHeizung L-M-H
+const unsigned char Box2_Regler = A5;  // SitzHeizung L-M-H   // 15-136
 const unsigned char Box2_Schloss = A4;  // Schott
 const unsigned char Box2_LED = 9;
 const unsigned char Box3_Button_gruen = 1;  // Evak
 const unsigned char Box3_Button_blau = A1; // gelb  Radium
 const unsigned char Box3_Schloss = A0;  // Gefahr
 const unsigned char Box3_LED = A3;
+
+struct SystemState{
+  unsigned char B1_Musik;
+  unsigned char B1_Vakuum;
+  unsigned char B1_Radon;
+  unsigned char B2_Schrott;
+  unsigned char B2_SitzHeizung;
+  unsigned char B2_Schott;  
+  unsigned char B3_Evak;
+  unsigned char B3_Radium;
+  unsigned char B3_Gefahr;
+};
+
+
+
+struct SystemState previous;
+
+
+/*
+ Extra function, we will need debouncing
+*/
+unsigned char read_button(unsigned char pin) 
+{
+  return digitalRead(pin);
+}
+
+/*
+ Extra function, we will need debouncing
+*/
+unsigned char read_slide(unsigned char pin) 
+{
+  return analogRead(pin);
+}
+
+
+
+/*
+Read the current state
+*/
+struct SystemState read_state() {
+  struct SystemState state;
+  
+  state.B1_Musik = read_button(Box1_Button_gruen);
+  state.B1_Vakuum = read_button(Box1_Button_blau);
+  state.B1_Radon = read_button(Box1_Schloss);
+  state.B2_Schrott = read_button(Box2_Button_gruen);
+  state.B2_Schott = read_button(Box2_Schloss);
+  state.B3_Evak = read_button(Box3_Button_gruen);
+  state.B3_Radium = read_button(Box3_Button_blau);
+  state.B3_Gefahr = read_button(Box3_Schloss);
+  
+  if  ((read_slide(Box2_Regler)) < 55)
+    state.B2_SitzHeizung = 1;
+  else if  ((read_slide(Box2_Regler)) < 90)
+    state.B2_SitzHeizung = 2;
+  else
+    state.B2_SitzHeizung = 3;
+  
+  return state;
+}
+
+/** Takes 2 states, if both are equal (with small flex. in analog) returns True
+*
+* return: True if states match, False else
+*/
+boolean state_matches(struct SystemState s1, struct SystemState s2)
+{}
+
+/** Compare the current state to 2 possible states
+*
+* return: 0: first state, 1: second state, 3: Wrong state
+**/
+unsigned char compare_state(struct SystemState prev, struct SystemState next)
+{
+
+}
 
 String inputString = "";         // a string to hold incoming data
 
@@ -99,12 +177,17 @@ void loop() {
     sprintf(buffer,"%4i",analogRead(Box2_Regler));
     lcd.print(buffer);
     
-    //digitalWrite(Box1_LED, digitalRead(Box3_Schloss));
-    //digitalWrite(Box2_LED, digitalRead(Box3_Button_gruen));
-    //digitalWrite(Box3_LED, digitalRead(Box3_Schloss));
-    digitalWrite(Box1_LED, HIGH);
-        digitalWrite(Box2_LED, LOW);
-            digitalWrite(Box3_LED, LOW);
+    digitalWrite(Box1_LED, digitalRead(Box3_Schloss));
+    digitalWrite(Box2_LED, digitalRead(Box3_Button_gruen));
+    digitalWrite(Box3_LED, digitalRead(Box3_Schloss));
+    
+    // Game goes here !
+    
+    // 1) Read the current state
+    
+    // 2) Randomize a job
+    
+    // 3) Check for expected change. If wrong change: broken
   }
   
   if (operationMode == broken)
