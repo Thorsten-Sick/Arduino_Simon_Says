7e2c3e438ac7f7fe76c33e6890a29488886cbb39
Thorsten Sick <thorsten.sick@email.de>
2014-09-19 20:32:14 +0200

    Names for buttons

--- Arduino_Simon_Says/Arduino_Simon_Says.ino
+++ Arduino_Simon_Says/Arduino_Simon_Says.ino
@@ -4,6 +4,16 @@ Those boxes are connected through ethernet cable (no ethernet protocol!) with up
 
 */
 
+
+/* Hardware test:
+
+LED 1 (pin 13)  OK
+LED 2 (pin 9) OK
+LED 3 (pin A3) OK
+
+LCD: ok
+
+*/
 // include the library code:
 #include <LiquidCrystal.h>
 // SmartMaker special
@@ -19,17 +29,17 @@ int taskCounter = 0;
 int brokenCounter = 0;
 boolean gameFinished = false;
 
-const unsigned char Box1_Button_gruen = 12; //gelb
-const unsigned char Box1_Button_blau = 11;
-const unsigned char Box1_Schloss = 10;
+const unsigned char Box1_Button_gruen = 12; //gelb: Musik
+const unsigned char Box1_Button_blau = 11;  // Vakuumeinlass
+const unsigned char Box1_Schloss = 10;  // Radon
 const unsigned char Box1_LED = 13;
-const unsigned char Box2_Button_gruen = 8;
-const unsigned char Box2_Regler = A5;
-const unsigned char Box2_Schloss = A4;
+const unsigned char Box2_Button_gruen = 8;  // Schrott abwurf
+const unsigned char Box2_Regler = A5;  // SitzHeizung L-M-H
+const unsigned char Box2_Schloss = A4;  // Schott
 const unsigned char Box2_LED = 9;
-const unsigned char Box3_Button_gruen = 1;
-const unsigned char Box3_Button_blau = A1; // gelb
-const unsigned char Box3_Schloss = A0;
+const unsigned char Box3_Button_gruen = 1;  // Evak
+const unsigned char Box3_Button_blau = A1; // gelb  Radium
+const unsigned char Box3_Schloss = A0;  // Gefahr
 const unsigned char Box3_LED = A3;
 
 String inputString = "";         // a string to hold incoming data
@@ -38,7 +48,7 @@ void setup() {
   // set up the LCD's number of columns and rows: 
   lcd.begin(16, 2);
   // Print a message to the LCD.
-  lcd.print("Simon says:");
+  lcd.print("Bugphalanx ReK:");
   
   pinMode(Box1_Button_gruen, INPUT_PULLUP);
   pinMode(Box1_Button_blau, INPUT_PULLUP);
@@ -89,9 +99,12 @@ void loop() {
     sprintf(buffer,"%4i",analogRead(Box2_Regler));
     lcd.print(buffer);
     
-    digitalWrite(Box1_LED, digitalRead(Box3_Schloss));
-    digitalWrite(Box2_LED, digitalRead(Box3_Button_gruen));
-    digitalWrite(Box3_LED, HIGH ); //digitalRead(Box3_Schloss));
+    //digitalWrite(Box1_LED, digitalRead(Box3_Schloss));
+    //digitalWrite(Box2_LED, digitalRead(Box3_Button_gruen));
+    //digitalWrite(Box3_LED, digitalRead(Box3_Schloss));
+    digitalWrite(Box1_LED, HIGH);
+        digitalWrite(Box2_LED, LOW);
+            digitalWrite(Box3_LED, LOW);
   }
   
   if (operationMode == broken)
